f5ff81bdb84ba3ee9fbcf51ce495405258f97d62
LeeJin <apple@appledeMacBook-Pro.local>
2015-12-17 09:52:43 +0800

    Initial Commit

---
+++ Autolayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Autolayout
+//
+//  Created by LeeJin on 15/12/17.
+//  Copyright (c) 2015年 psylife. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Autolayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Autolayout
+//
+//  Created by LeeJin on 15/12/17.
+//  Copyright (c) 2015年 psylife. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
