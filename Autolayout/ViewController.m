fe2efb8f7c2c06c8dea99182b653710440adf511
LeeJin <apple@appledeMacBook-Pro.local>
2015-12-21 11:33:15 +0800

    Autolayout

--- Autolayout/ViewController.m
+++ Autolayout/ViewController.m
@@ -7,8 +7,17 @@
 //
 
 #import "ViewController.h"
-
+#import "YCDefine.h"
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *CenterY;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *userNameSpace;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *paswordSpace;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *loginBtnSpace;
+
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *userNameAndPasswordHeight;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *loginBtnHeight;
+
 
 @end
 
@@ -17,6 +26,29 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    if (YC_IS_IPHONE4) {
+        self.CenterY.constant = 120;
+        self.userNameSpace.constant  = 15;
+        self.paswordSpace.constant   = 15;
+        self.loginBtnSpace.constant  = 15;
+    }else if (YC_IS_IPHONE5){
+        self.CenterY.constant = 160;
+        self.userNameSpace.constant  = 30;
+        self.paswordSpace.constant   = 30;
+        self.loginBtnSpace.constant  = 30;
+    }else if (YC_IS_IPHONE6){
+        self.CenterY.constant = 200;
+        self.userNameSpace.constant  = 40;
+        self.paswordSpace.constant   = 40;
+        self.loginBtnSpace.constant  = 40;
+    }else if (YC_IS_IPHONE6_PLUS){
+        self.CenterY.constant = 240;
+        self.userNameAndPasswordHeight.constant = 55;
+        self.loginBtnHeight.constant = 65;
+        self.userNameSpace.constant  = 50;
+        self.paswordSpace.constant   = 50;
+        self.loginBtnSpace.constant  = 50;
+    }
 }
 
 - (void)didReceiveMemoryWarning {
